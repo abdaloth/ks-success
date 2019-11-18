369b6e46dfbc8bc2e0331a0c07fec49ccc7cdb6e
abdaloth <abdullahoalothman@gmail.com>
2019-11-18 10:50:45 -0500

    changed nan/error handling.fill only dict features with empty dict, else fill with empty string.print error message before logging erroneous file

--- filter_data.py
+++ filter_data.py
@@ -8,22 +8,30 @@ tqdm.pandas()
 
 gzip_list = glob('original_data/*.gz')
 
+
 def filter_df(json_df, fname=None):
-    df_list = []
+    drop_cols = ['urls',
+                 'profile',
+                 'photo',
+                 'category',
+                 'creator',
+                 'slug',
+                 'location']
 
+    dict_features = ['creator', 'profile', 'category', 'location']
     t = pd.DataFrame.from_records(json_df['data'])
-
-    for feature in ['creator', 'profile', 'category', 'location']:
+    
+    for feature in dict_features:
         if not feature in t.columns:
             t[feature] = {}
             pass
         pass
     
-    t = t.applymap(lambda x: {} if np.all(pd.isnull(x)) else x)
-
+    t.loc[:,dict_features] = t[dict_features].applymap(lambda x: {} if np.all(pd.isnull(x)) else x)
+    t = t.applymap(lambda x: '' if np.all(pd.isnull(x)) else x)
     t['creator_id'] = t['creator'].apply(lambda c: c.get('id', None))
     t['creator_name'] = t['creator'].apply(lambda c: c.get('name', None))
-    
+
     t['profile_id'] = t['profile'].apply(lambda c: c.get('id', None))
     t['project_id'] = t['profile'].apply(lambda c: c.get('project_id', None))
     t['profile_state'] = t['profile'].apply(lambda c: c.get('state', None))
@@ -36,27 +44,28 @@ def filter_df(json_df, fname=None):
     t['location_type'] = t['location'].apply(lambda c: c.get('type', None))
     t['location_country'] = t['location'].apply(lambda c: c.get('country', None))
 
-    t = t.drop(columns=['urls', 'profile', 'photo', 'category', 'creator', 'slug', 'location'])
-    df_list.append(t)
+    t = t.drop(columns=drop_cols)
 
     if(fname):
-        pd.concat(df_list, sort=False, ignore_index=True).to_csv(fname, index=False)
+        t.to_csv(fname, index=False)
     else:
-        return df_list
+        return t
 
 
 if __name__ == '__main__':
     done_files = glob('filtered_data/*.csv')
     done_files = [p.split('\\')[1] for p in done_files]
-    for path in tqdm(gzip_list[1:]):
+    for path in tqdm(gzip_list):
         with gzip.open(path) as gzip_file:
             fname = f"filtered_data/KS_{path.split('_')[2].split('.')[0]}.csv"
             if fname.split('/')[1] in done_files:
                 continue
             try:
-                filter_df(pd.read_json(gzip_file, orient='records', lines=True), fname)
+                df = pd.read_json(gzip_file, orient='records', lines=True)
+                filter_df(df, fname)
                 pass
-            except:
+            except Exception as e:
+                print(e)
                 with open('errorlog', 'a') as f:
                     f.write(path+'\n')
                     pass
